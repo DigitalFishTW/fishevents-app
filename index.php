147d541b75c49d062186bd3a18c4c1129b3206c4
Hiram Huang <chown9835@gmail.com>
2016-04-22 20:26:42 +0000

    Rewrited all the templates, added signin, signup, sidemenu.

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php include 'php/templates/header.php'; ?>
+    
+<div class="ui text container">
+    <p>&nbsp;</p>
+    <div class="ui segment" style="max-width: 350px; margin: 0 auto">
+        <h1 class="ui center aligned header">
+            Welcome Back!
+            <div class="sub header">We're always here.</div>
+        </h1>
+        <form class="ui form">
+            <div class="field">
+                <label>Username</label>
+                <input type="text" name="username">
+            </div>
+            <div class="field">
+              <label>Password<a href="user/forgot-password.php" style="float: right">Forgot password?</a></label>
+              <input type="password" name="password">
+            </div>
+            <button class="ui fluid positive button" type="submit">Sign in</button>
+        </form>
+    </div>
+</div>
+    
+<?php include 'php/templates/footer.php'; ?>
+    
---
+++ php/templates/header.php
@@ -0,0 +1,57 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Fish Events</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="js/jquery-2.2.3.min.js"></script>
+<script src="css/Semantic-UI/dist/semantic.min.js"></script>
+<link rel="stylesheet" type="text/css" href="css/Semantic-UI/dist/semantic.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script>
+$(function()
+{
+    $('.ui.dropdown').dropdown()
+})
+    
+</script>
+</head>
+<body>
+    <!-- 左側選單 -->
+    <aside>
+        <div class="ui large fluid vertical inverted blue menu">
+            <a class="item">
+                <i class="icon dashboard"></i>
+                Dashboard
+            </a>
+            <a class="item">
+                <i class="icon flag"></i>
+                Journeys
+            </a>
+            <a class="item">
+                <i class="icon trophy"></i>
+                Stocks
+            </a>
+            <a class="item">
+                Identifiers
+            </a>
+        </div>
+    </aside>
+    <!-- / 左側選單 -->
+    
+    <div id="outer">
+        
+        <!-- 置頂選單 -->
+        <nav>
+            <ul>
+                <li class="brand">Fish Events</li>
+                <li class="right">
+                    <img class="ui avatar image" src="images/fake-avatar.png">
+                    <span>Yami Odymel</span>
+                </li>
+            </ul>
+        </nav>
+        <!-- / 置頂選單 -->
+        
+        
+        <!-- 主要內容容器 -->
+        <section>
